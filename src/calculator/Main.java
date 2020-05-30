f3b94202428f87eac8f14afa7ac4373246ff389d
Elena <reikon9@rambler.ru>
2020-05-30 23:51:46 +0300

    Add calculator - homework to lesson_4

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Calculator");
-        primaryStage.setScene(new Scene(root, 250, 300));
+        primaryStage.setScene(new Scene(root, 240, 300));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
